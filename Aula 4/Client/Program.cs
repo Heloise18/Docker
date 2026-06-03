0411067283393e1e83929c61ce4a9575bd617879
Heloise18 <heloisezelma18@gmail.com>
2026-06-03 08:46:51 -0300

    aaaa

---
+++ Aula 4/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var http = new HttpClient();
+
+while (true)
+{
+    try
+    {
+        var response = await http.GetStringAsync(
+            "http://maestro"
+        );
+
+        Console.WriteLine(response);
+    }
+    catch (HttpRequestException ex)
+    {
+        Console.WriteLine($"Erro na requisição: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro inesperado: {ex.Message}");
+    }
+
+    await Task.Delay(1000);
+}
